2fe9a81ebf537044d045e8775f42d6ac95d693fd
anand8533 <anand8533@gmail.com>
2024-06-01 16:23:24 -0400

    1

---
+++ src/main/java/com/seeker/BootJpaProj04_JpaRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.seeker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJpaProj04_JpaRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJpaProj04_JpaRepositoryApplication.class, args);
+	}
+
+}
